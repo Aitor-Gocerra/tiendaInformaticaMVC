3e880570180c6ad0c837b6ea08ead6588addb3a8
Aitor-Gocerra <aitorgomezcerrato.guadalupe@alumnado.fundacionloyola.net>
2025-12-01 16:18:05 +0100

    Borrado de archivos que no necesito

--- index.php
+++ index.php
@@ -1,32 +1,32 @@
 <?php
     require_once __DIR__ . '/configIndex.php';
 
-    if(!isset($_GET['c'])){
+    if (!isset($_GET['c'])) {
         $_GET['c'] = CONTROLADOR_DEFECTO;
     }
 
-    if(!isset($_GET['m'])){
+    if (!isset($_GET['m'])) {
         $_GET['m'] = MODELO_DEFECTO;
     }
 
     $rutaControlador = RUTA_CONTROLADORES . $_GET['c'] . '.php';
-    
+
     require_once $rutaControlador;
-        
+
     $controlador = 'Con' . $_GET['c'];
     $objControlador = new $controlador();
 
     $datos = []; /* Para guardar los datos que obtenga del controlador */
 
-    if(method_exists($objControlador, $_GET['m'])){
+    if (method_exists($objControlador, $_GET['m'])) {
         $datos = $objControlador->{$_GET['m']}();
     }
 
-    if($objControlador->vista != ''){
-        if(is_array($datos)){
+    if ($objControlador->vista != '') {
+        if (is_array($datos)) {
             extract($datos);
+        }
+        require_once RUTA_VISTAS . $objControlador->vista . '.php';
     }
-    require_once RUTA_VISTAS . $objControlador->vista . '.php';
-}
 
 ?>

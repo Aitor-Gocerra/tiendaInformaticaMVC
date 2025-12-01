b5acd6475cbc439a8ab4d1f092650d796f5ccd36
Aitor-Gocerra <aitorgomezcerrato.guadalupe@alumnado.fundacionloyola.net>
2025-12-01 17:27:03 +0100

    Tabulaciones

--- configIndex.php
+++ configIndex.php
@@ -1,8 +1,7 @@
 <?php
-
     DEFINE('RUTA_VISTAS', 'Vistas/');
     DEFINE('RUTA_CONTROLADORES', 'Controladores/');
+    
     DEFINE('CONTROLADOR_DEFECTO', 'Ordenadores');
     DEFINE('METODO_DEFECTO', 'listarOrdenadores');
-
 ?>
--- index.php
+++ index.php
@@ -1,32 +1,32 @@
 <?php
-require_once __DIR__ . '/configIndex.php';
+    require_once __DIR__ . '/configIndex.php';
 
-if (!isset($_GET['c'])) {
-    $_GET['c'] = CONTROLADOR_DEFECTO;
-}
-
-if (!isset($_GET['m'])) {
-    $_GET['m'] = METODO_DEFECTO;
-}
-
-$rutaControlador = RUTA_CONTROLADORES . $_GET['c'] . '.php';
-
-require_once $rutaControlador;
-
-$controlador = 'Con' . $_GET['c'];
-$objControlador = new $controlador();
-
-$datos = []; /* Para guardar los datos que obtenga del controlador */
-
-if (method_exists($objControlador, $_GET['m'])) { // Comprueba si el controlador tiene el metodo
-    $datos = $objControlador->{$_GET['m']}(); // Cojo los datos
-}
-
-if ($objControlador->vista != '') { // Comprueba si el controlador tiene una vista
-    if (is_array($datos)) { // Comprueba si el controlador tiene datos
-        extract($datos);
+    if (!isset($_GET['c'])) {
+        $_GET['c'] = CONTROLADOR_DEFECTO;
     }
-    require_once RUTA_VISTAS . $objControlador->vista . '.php';
-}
+
+    if (!isset($_GET['m'])) {
+        $_GET['m'] = METODO_DEFECTO;
+    }
+
+    $rutaControlador = RUTA_CONTROLADORES . $_GET['c'] . '.php';
+
+    require_once $rutaControlador;
+
+    $controlador = 'Con' . $_GET['c'];
+    $objControlador = new $controlador();
+
+    $datos = []; /* Para guardar los datos que obtenga del controlador */
+
+    if (method_exists($objControlador, $_GET['m'])) { // Comprueba si el controlador tiene el metodo
+        $datos = $objControlador->{$_GET['m']}(); // Cojo los datos
+    }
+
+    if ($objControlador->vista != '') { // Comprueba si el controlador tiene una vista
+        if (is_array($datos)) { // Comprueba si los datos son un array y extrae dichos datos en forma de array asociativo para trabajar con variables claras
+            extract($datos);
+        }
+        require_once RUTA_VISTAS . $objControlador->vista . '.php';
+    }
 
 ?>
